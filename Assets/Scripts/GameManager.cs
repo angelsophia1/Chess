2b144c30859ac56ef50e1732c202f402821205d2
angelsophia1 <angelsunyulei@126.com>
2019-09-18 16:55:58 +0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public Board mBoard;
+    public PieceManager mPieceManager;
+
+    void Start()
+    {
+        mBoard.Create();
+        mPieceManager.Setup(mBoard);
+    }
+}
---
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pawn : BasePiece
+{
+    public override void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        base.Setup(newTeamColor, newSpriteColor, newPieceManager);
+        mMovement = mColor == Color.white ? new Vector3Int(0, 1, 1) : new Vector3Int(0,-1,-1);
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Pawn");
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+        CheckForPromotion();
+    }
+
+    private bool MatchesState(int targetX, int targetY, CellState targetState)
+    {
+        CellState cellState = CellState.None;
+        cellState = mCurrentCell.mBoard.ValidateCell(targetX,targetY,this);
+        if (cellState == targetState)
+        {
+            mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+            return true;
+        }
+        return false;
+    }
+
+    private void CheckForPromotion()
+    {
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+        CellState cellState = mCurrentCell.mBoard.ValidateCell(currentX, currentY+mMovement.y,this);
+        if (cellState==CellState.OutOfBounds)
+        {
+            Color spriteColor = GetComponent<Image>().color;
+            mPieceManager.PromotePiece(this, mCurrentCell,mColor,spriteColor);
+        }
+    }
+
+    protected override void CheckPathing()
+    {
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+        MatchesState(currentX - mMovement.z, currentY + mMovement.z, CellState.Enemy);
+        if (MatchesState(currentX,currentY+mMovement.y,CellState.Free))
+        {
+            if (mIsFirstMove)
+            {
+                MatchesState(currentX, currentY + (mMovement.y * 2), CellState.Free);
+            }
+        }
+        MatchesState(currentX + mMovement.z, currentY + mMovement.z, CellState.Enemy);
+    }
+}
